256e95538c8cdcbffccdde749e57a1509b1bdf76
MRSgkce <mursidegokce985@gmail.com>
2024-08-12 11:56:04 +0300

    15'den geri sayıyor. Süre bitince süre bitti yaıyor.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var timer: Timer?
+    var kalanZaman = 15
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "zaman \(kalanZaman)"
     }
 
 
+    @IBAction func buton(_ sender: Any) {
+        if timer != nil {
+                    timer?.invalidate()
+                }
+                
+                kalanZaman = 15
+                label.text = "zaman \(kalanZaman)"
+                
+                print("Timer başlatılıyor")
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFonk), userInfo: nil, repeats: true)
+    }
+    @objc func timerFonk() {
+            print("Timer çalışıyor")
+            if kalanZaman > 0 {
+                kalanZaman -= 1
+                label.text = "zaman \(kalanZaman)"
+            } else {
+                label.text = "süre bitti"
+                timer?.invalidate()
+                timer = nil
+            }
+        }
 }
 
